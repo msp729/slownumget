d6415f3fbfde41d68dd0d90564cf12aa5f24001a
msp729 <n.b.nrb99@gmail.com>
2021-05-06 22:18:45 -0500

    0.1.1, might be slightly faster, still uses same method of getting values, might work better on more systems.

--- slownumget/slownumget.py
+++ slownumget/slownumget.py
@@ -1,7 +1,7 @@
 import subprocess
 def isPositive(num):
     return num == abs(num)
-zero = lambda: subprocess.call('exit',shell=True)
+zero = lambda: subprocess.call('',shell=True)
 def get(num=zero()):
     out = zero()
     if isPositive(num):
